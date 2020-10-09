b9afd9685888cd75cffcec3178a50951d0e98fa4
holsety_s <muspuzic@gmail.com>
2020-10-09 14:43:23 +0200

    add tilemap library of predefined rooms

---
+++ js/tilemap.js
@@ -0,0 +1,3 @@
+var tileMap = [
+  [["Mud","Mud","Mud","Mud","Mud","Mud","Mud","Mud","Mud"],["Mud","Wall","Wall","Pit","Pit","Pit","Wall","Wall","Mud"],["Mud","Wall","Water","Water","Water","Water","Water","Wall","Mud"],["Mud","Pit","Water","Trap","Trap","Trap","Water","Pit","Mud"],["Mud","Floor","Water","Trap","Chest","Trap","Water","Pit","Mud"],["Mud","Pit","Water","Trap","Trap","Trap","Water","Pit","Mud"],["Mud","Wall","Water","Water","Water","Water","Water","Wall","Mud"],["Mud","Wall","Wall","Pit","Pit","Pit","Wall","Wall","Mud"],["Mud","Mud","Mud","Mud","Mud","Mud","Mud","Mud","Mud"]]
+];
