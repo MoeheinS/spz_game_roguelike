9d9f1c4350fc3bd944e832571d5851dad8c5a056
holsety_s <muspuzic@gmail.com>
2021-01-10 09:36:30 +0100

    move messages to own file

---
+++ js/messages.js
@@ -0,0 +1,40 @@
+class Message {
+  constructor(message, hidden) {
+    this.message = message;
+    this.hidden = hidden || false;
+    game_state.message_history.unshift(this);
+    console.log(`%c${(this.hidden ? 'DEBUG: ':'')}${this.message}`,'color:#5CC0FA;font-family:Comic Sans MS;');
+
+    Message.updateDOM(this);
+  }
+  static latest(all) {
+    let latest = ( all ? game_state.message_history : game_state.message_history.filter(t => !t.hidden) );
+    if( latest.length ){
+      return latest[0].message;
+    }else{
+      return 'No messages to show';
+    }
+  }
+  static list(all) {
+    let listed = ( all ? game_state.message_history : game_state.message_history.filter(t => !t.hidden) );
+    console.table(listed);
+    return listed;
+  }
+  static wipe() {
+    game_state.message_history = [];
+    Message.updateDOM(false,true);
+  }
+  static updateDOM(m, clear) {
+    if( clear ){
+      dom_messageWindow.querySelector('.window__title__text').innerText = 'Message history';
+      return dom_messageWindow.querySelector('.window__body').innerHTML = '';
+    }
+    if( m.hidden ){ // TODO: implement wizardmode toggle which lets these messages get logged too
+      return;
+    }
+    dom_messageWindow.querySelector('.window__title__text').innerText = Message.latest(false);
+    let p = document.createElement('p');
+        p.innerText = m.message;
+    dom_messageWindow.querySelector('.window__body').append(p);
+  }
+}
